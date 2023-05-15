07ce2c010f55690c53722b2ca3d947828a0150a2
haradhansharma <haradhan.sharma@gmail.com>
2023-05-15 17:56:12 +0600

    setings

--- veerera/settings/settings_email.py
+++ veerera/settings/settings_email.py
@@ -1,4 +1,4 @@
-from . import os
+import os
 
 
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
@@ -8,5 +8,5 @@ EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS=False
 EMAIL_USE_SSL=True
-import ast
-ADMINS = ast.literal_eval(os.getenv('ADMINS'))
+# import ast
+# ADMINS = ast.literal_eval(os.getenv('ADMINS'))
