655e8790522579df9051472f07664309e7d67687
haradhansharma <haradhan.sharma@gmail.com>
2023-05-15 23:30:44 +0600

    setings

--- veerera/settings/settings_database.py
+++ veerera/settings/settings_database.py
@@ -5,9 +5,9 @@ DEBUG = ast.literal_eval(os.getenv('DEBUG', 'False'))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'veereraorg' if DEBUG else os.getenv('DB_NAME'),
-        'USER': 'root' if DEBUG else os.getenv('DB_USER'),
-        'PASSWORD': '' if DEBUG else os.getenv('DB_PASS'),
+        'NAME': 'veereraorg' if DEBUG else 'veerera_main',
+        'USER': 'root' if DEBUG else 'veerera_haradha',
+        'PASSWORD': '' if DEBUG else 'Y3aZPODkHrvJ',
         'HOST':'localhost',
         'PORT': '3306',
         'OPTIONS': {
