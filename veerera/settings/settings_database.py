3e34892e63947c7f2c14dcbca494502f799834a3
haradhansharma <haradhan.sharma@gmail.com>
2023-05-15 22:26:12 +0600

    setings

--- veerera/settings/settings_database.py
+++ veerera/settings/settings_database.py
@@ -1,36 +1,22 @@
 import os
 import ast
 DEBUG = ast.literal_eval(os.getenv('DEBUG', 'False'))
-print(DEBUG)
-print(type(DEBUG))
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'veereraorg',
-            'USER': 'root',
-            'PASSWORD': '',
-            'HOST':'localhost',
-            'PORT': '3306',
-            'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'veereraorg' if DEBUG else os.getenv('DB_NAME'),
+        'USER': 'root' if DEBUG else os.getenv('DB_USER'),
+        'PASSWORD': '' if DEBUG else os.getenv('DB_PASS'),
+        'HOST':'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASS'),
-            'HOST':'localhost',
-            'PORT': '3306',
-            'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            }
-        }
-    }
+}
+
+    
     
 
 
